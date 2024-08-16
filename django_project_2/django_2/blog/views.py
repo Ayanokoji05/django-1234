efda4e47870a0dd84d9de81e34fe7ed71842b44b
Ayanokoji05 <pratushkumarpusti@gmail.com>
2024-08-16 18:11:36 +0530

    added new project : django_project_2

---
+++ django_project_2/django_2/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+
+def home(request):
+    return render(request,'blog/home.html')
+
+def about(request):
+    return render(request,'blog/about.html')
+
+# Create your views here.
